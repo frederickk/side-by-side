93b24a65464b94bc081d89cda2590dbc36d136a9
ken frederick <ken.frederick@gmx.de>
2018-02-17 20:50:47 -0800

    ♻️ Refactored HTML to reflect changes in build approach as well as implemented service-worker

---
+++ src/service-worker.js
@@ -0,0 +1,76 @@
+// Lifted nearly verbatim from https://raw.githubusercontent.com/google-developer-training/pwa-training-labs/master/cache-api-lab/solution/service-worker.js
+
+(function() {
+  'use strict';
+
+  /**
+   * Files and URLs to cache
+   * @type {Array}
+   */
+  const filesToCache = [
+    'https://fonts.googleapis.com/css?family=Material+Icons|Space+Mono&subset=latin',
+  ];
+
+  /**
+   * Name of the cache
+   * @type {string}
+   */
+  const staticCacheName = 'side-by-side-cache';
+
+  /**
+   * Install service worker and cache static assets
+   * @type {[type]}
+   */
+  self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(caches.open(staticCacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    }));
+  });
+
+  /**
+   * Fetch cached assets
+   * @type {[type]}
+   */
+  self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(caches.match(event.request).then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request).then(response => {
+        return caches.open(staticCacheName).then(cache => {
+          if (event.request.url.indexOf('test') < 0) {
+            cache.put(event.request.url, response.clone());
+          }
+          return response;
+        });
+      });
+    }).catch(function(error) {
+      console.log('Error, ', error);
+    }));
+  });
+
+  /**
+   * Activate/create service worker
+   * @type {[type]}
+   */
+  self.addEventListener('activate', event => {
+    console.log('Activating new service worker...');
+
+    const cacheWhitelist = [staticCacheName];
+
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        }));
+      })
+    );
+  });
+
+})();
